cae472e660fe19826f5c08211fbd06059104eb69
viewnext08_ik <inna_ik@yahoo.es>
2019-06-24 13:03:53 +0200

    add Person

---
+++ src/main/java/com/test/boot/helloJPA/CustomResponse.java
@@ -0,0 +1,9 @@
+package com.test.boot.helloJPA;
+
+public class CustomResponse {
+    private String message;
+    private boolean success;
+    private Iterator<Person> peronas;
+
+
+}
--- src/main/java/com/test/boot/helloJPA/MainController.java
+++ src/main/java/com/test/boot/helloJPA/MainController.java
@@ -4,32 +4,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(path="/person")
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="/persons")
 public class MainController {
 
     @Autowired
     private PersonRepository personRepository;
 
     //@RequestMapping(method = RequestMethod.GET.path="/all")
-    @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Person> getAllPersons() {
+    //@GetMapping(path = "/all")
+    //@GetMapping(path = "/")
+    @GetMapping
+    public Iterable<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+    @GetMapping(path = "/json")
+    public CustomResponse getAllPersonsJSON() {
         return personRepository.findAll();
     }
 
-    @GetMapping(path = "/search")
-    public @ResponseBody Person getPerson(@RequestParam Integer id) {
-        Person p = (Person)personRepository.findById(id);
-        return p;
+    //BUSCA: GET
+    @GetMapping(path = "/{id}")
+    public  Person getPerson(
+            HttpServletResponse response,
+            @PathVariable(name="id") Integer id) throws IOException {
+        Optional<Person> p = personRepository.findById(id);
+        if(p.isPresent()) {
+            return p.get();
+        }
+        response.sendError(404, "Id no encontrado");
+        return null;
     }
-
-    @PostMapping(path = "/add")
-    public @ResponseBody String addPerson(
+    //AÑADIR UNO: POST
+    //@PostMapping(path = "/add")
+    @PostMapping
+    public  Person addPerson(
+            HttpServletResponse response,
             @RequestParam String firstName,
             @RequestParam String lastName,
             @RequestParam String email,
             @RequestParam Integer edad
-    ) {
+    ) throws IOException{
 
         Person p = new Person();
         p.setFirstName(firstName);
@@ -38,7 +57,48 @@ public class MainController {
         p.setEdad(edad);
 
         personRepository.save(p);
-        return "OK";
+        response.setStatus(201);
+        response.setHeader("location","/person/"+ p.getId());
+        return p;
+    }
+    //ACTUALIZA: PUT
+    @PutMapping(path = "/{id}")
+    public  Object updatePerson(
+            HttpServletResponse response,
+            @PathVariable(name="id")  Integer id,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam String email,
+            @RequestParam Integer edad
+
+    ) throws IOException {
+       Optional<Person> p = personRepository.findById(id);
+       if(p.isPresent()) {
+           Person np = p.get();
+           np.setEdad(edad);
+           np.setEmail(email);
+           np.setFirstName(firstName);np.setLastName(lastName);
+           personRepository.save(np);
+           response.setStatus(201);
+           return p;
+        }
+        response.sendError(404, "Id no encontrado");
+        return null;
+    }
+    //BORRA: DELETE
+    @DeleteMapping(path = "/{id}")
+    public Object delPerson(
+            HttpServletResponse response,
+            @PathVariable(name="id")  Integer id
+    ) throws IOException {
+        Optional<Person> p = personRepository.findById(id);
+        if(p.isPresent()) {
+            personRepository.delete(p.get());
+            response.setStatus(204);
+            return null;
+        }
+        response.sendError(404, "Id no encontrado");
+        return null;
     }
 }
 
