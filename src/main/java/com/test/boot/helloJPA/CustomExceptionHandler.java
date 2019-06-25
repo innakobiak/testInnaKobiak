dca44da1758930a4b9eb792a3f4a96cdbb58b94f
viewnext08_ik <inna_ik@yahoo.es>
2019-06-25 12:14:47 +0200

    add Person

--- src/main/java/com/test/boot/helloJPA/CustomExceptionHandler.java
+++ src/main/java/com/test/boot/helloJPA/CustomExceptionHandler.java
@@ -6,16 +6,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import java.io.IOException;
 
 @ControllerAdvice
 public class CustomExceptionHandler  extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler ({PersonNotFoundExcception.class,PhoneNotFoundExcception.class})
+    @ExceptionHandler ({PersonNotFoundExcception.class})
     public @ResponseBody
     CustomResponse springHandleNotFound(HttpServletResponse resp, PersonNotFoundExcception e) throws IOException {
         resp.setStatus(404);
         return new CustomResponse( e.getLocalizedMessage(),false);
         //resp.sendError(404, "No Encontrado");
     }
+
+    @ExceptionHandler (ConstraintViolationException.class)
+    public @ResponseBody
+    CustomResponse springHandleValidationException(HttpServletResponse resp, Exception e) throws IOException {
+        resp.setStatus(500);
+        return new CustomResponse( e.getLocalizedMessage(),false);
+        //resp.sendError(404, "No Encontrado");
+    }
+
 }
---
+++ src/main/java/com/test/boot/helloJPA/PersonValidationException.java
@@ -0,0 +1,8 @@
+package com.test.boot.helloJPA;
+
+
+import javax.validation.ValidationException;
+
+public class PersonValidationException extends ValidationException {
+
+}
