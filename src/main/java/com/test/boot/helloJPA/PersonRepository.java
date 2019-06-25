8957d7131d07c6a2e5af106b487c2785815ba216
viewnext08_ik <inna_ik@yahoo.es>
2019-06-25 13:30:13 +0200

    añadir @Projection & excerpt

--- src/main/java/com/test/boot/helloJPA/PersonRepository.java
+++ src/main/java/com/test/boot/helloJPA/PersonRepository.java
@@ -1,9 +1,12 @@
 package com.test.boot.helloJPA;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+//@RepositoryRestResource(path = "personas", excerptProjection = InlinePhones.class)
+@RepositoryRestResource( excerptProjection = InlinePhones.class)
 public interface PersonRepository extends CrudRepository<Person,Integer> {
 }
--- src/main/java/com/test/boot/helloJPA/PersonValidationException.java
+++ src/main/java/com/test/boot/helloJPA/PersonValidationException.java
@@ -3,6 +3,7 @@ package com.test.boot.helloJPA;
 
 import javax.validation.ValidationException;
 
+//TODO: borrar ,sin uso
 public class PersonValidationException extends ValidationException {
 
 }
